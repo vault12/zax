1a138ff5756ea1509d74bbc4294f62cf4c7e83d1
Max Skibinsky <max@skibinsky.com>
2015-04-29 19:37:48 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,32 @@
+source 'https://rubygems.org'
+ruby "2.2.2"
+
+gem 'rails', '4.2.1'
+gem 'rails-api'
+gem 'spring', :group => :development
+
+group :development do
+  gem 'sqlite3'
+end
+
+group :production do
+  gem 'pg'
+end
+
+gem 'dalli'
+gem 'kgio'
+gem 'unicorn'
+
+gem 'rbnacl-libsodium'
+
+# To use ActiveModel has_secure_password
+# gem 'bcrypt', '~> 3.1.7'
+
+# To use Jbuilder templates for JSON
+# gem 'jbuilder'
+
+# Deploy with Capistrano
+# gem 'capistrano', :group => :development
+
+# To use debugger
+# gem 'ruby-debug19', :require => 'ruby-debug'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+Zax::Application.config.secret_token = 'a6317c8c9e7da13b0e400fd562985bfff8cb8007281dd6fb04829050aea76add246e808dddb9596bb5c42890793dc15204c8248fda9d05535029af443dc7a37e'
