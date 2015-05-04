ab7af1c18f2b68893e09c75ca609722e55ebbf75
Max Skibinsky <max@skibinsky.com>
2015-05-04 03:23:37 -0700

    starting on session handshake

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'session' => 'session#new_session_token'
+  post 'session' => 'session#verify_session_token'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
