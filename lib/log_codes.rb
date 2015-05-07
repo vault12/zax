358e2526b72c576587ecc8ddabc87315d1fab193
Max Skibinsky <max@skibinsky.com>
2015-05-06 18:11:43 -0700

    tweak codes

--- lib/log_codes.rb
+++ lib/log_codes.rb
@@ -1,8 +1,18 @@
 module LogCodes
   # log file prefix codes
+
+  # low level info, can be ignored
   INFO      = "\xF0\x9F\x94\xA9 "
+
+  # low level info, can be ignored, minor positve on the protocol path
   INFO_GOOD = "\xF0\x9F\x94\x91 "
-  INFO_NEG  = "\xE2\x80\xA0"
-  WARN      = "\xF0\x9F\x94\xBB"
+
+  # low level info, can be ignored, minor deviation from the protocol path
+  INFO_NEG  = "\xF0\x9F\x94\xBB "
+
+  # a noteworthy event, a non-breaking deviation from the protocol path
+  WARN      = "\xE2\x80\xA0"
+
+  # important, breaking event, must be investigated.
   ERROR     = "\xE2\x9D\x97\xEF\xB8\x8F \xE2\x98\xA2"
 end
