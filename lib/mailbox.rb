8c78331ed96ee2abde8b6d3f5cfe46bce7447553
Max Skibinsky <max@skibinsky.com>
2015-06-05 03:01:50 -0700

    Add Mailbox from and id fields, Mailbox tests
    Mailbox stores blind crypto data. Owner of :hpk will need 'from' field to look up local
    public key corresponding to 'from' to decrypt crypto data received from relay mailbox.
    Since delivery is unreliable client and Mailbox need some sort of ID to refer to crypto
    data. However, we can not use incremental ID since it will leak information, such as
    total number of messages sent by owner of HPK. Therefore we generate random id
    with very little chance of collision.

--- lib/mailbox.rb
+++ lib/mailbox.rb
@@ -1,33 +1,72 @@
 class Mailbox
-  TIMEOUT = Rails.configuration.x.relay.mailbox_timeout
-
-  attr_accessor :hpk,:count
+  include Utils
+  attr_accessor :hpk,:top
 
   def initialize(hpk)
     raise "Wrong HPK in mailbox.ctor()" unless hpk and hpk.length==32
+    @tmout = Rails.configuration.x.relay.mailbox_timeout
     @hpk = hpk
-    @count = Rails.cache.fetch("count_#{@hpk}", expires_in: TIMEOUT) { 0 }
+    @top = Rails.cache.fetch("top_#{@hpk}", expires_in: @tmout) { 0 }
   end
 
-  def store(data)
+  # --- Store crypto records ---
+
+  # Records are always added above the top mark
+  def store(from, data)
     raise "No data in mailbox.store()" unless data
-    Rails.cache.write("data_#{@count}_#{@hpk}",data,expires_in: TIMEOUT)
-    @count+=1
-    Rails.cache.write("count_#{@hpk}",@count,expires_in: TIMEOUT)
+    item = {
+      # Something resonably unique during 3-day expiration window
+      id: RbNaCl::Random.random_bytes(16),
+      from: from,
+      data: data
+    }
+    Rails.cache.write("item_#{@top}_#{@hpk}", item, expires_in: @tmout)
+    @top+=1
+    Rails.cache.write("top_#{@hpk}", @top, expires_in: @tmout)
+    return item
   end
 
+  # --- Read crypto records ---
   def read(idx)
-    Rails.cache.read "data_#{idx}_#{@hpk}"
+    Rails.cache.read "item_#{idx}_#{@hpk}"
   end
 
   def read_all
     a = []
-    for i in (0..@count-1)
-      data = self.read i
-      yield data if block_given?
-      a.push data
+    for i in (0..@top-1)
+      item = self.read i
+      next unless item
+      yield item if block_given?
+      a.push item
     end
     return a
+  end
+
+  # returns [index, item] by id
+  def find_by_id(id)
+    for i in (0..@top-1)
+      item = self.read i
+      next unless item
+      return i,item if item[:id].eql?(id)
+    end
+  end
+
+  # --- Delete crypto records ---
+  def delete(idx)
+    Rails.cache.delete "item_#{idx}_#{@hpk}"
+    update_top
+  end
+
+  def delete_by_id(id)
+    delete find_by_id(id).first
+  end
+
+  def update_top
+    ts = @top
+    while @top>0 and not read(@top-1)
+      @top-=1
+    end
+    Rails.cache.write("top_#{@hpk}",@top,expires_in: @tmout) if ts!=@top
   end
 
 end
--- test/integration/mailbox_test.rb
+++ test/integration/mailbox_test.rb
@@ -7,13 +7,51 @@ class MailboxTest < ActionDispatch::IntegrationTest
     mbx = Mailbox.new hpk
 
     assert_not_nil mbx.hpk
-    assert_equal 0,mbx.count
+    assert_equal 0,mbx.top
 
-    mbx.store "hello"
-    assert_equal 1,mbx.count
+    for i in (1..5)
+      mbx.store h2("#{i}"), "hello_N#{i}"
+      assert_equal i,mbx.top
+    end
 
-    mbx.store "world"
-    assert_equal 2,mbx.count
+    # [1,2,3,4,5]
+    results = mbx.read_all
+    assert_equal 5,results.length
+    assert_equal 5,mbx.top
+
+    id1 = results[1][:id]
+    id3 = results[3][:id]
+
+    assert_equal results[1], mbx.find_by_id(id1).second
+    assert_equal results[3], mbx.find_by_id(id3).second
+
+    # [1,nil,3,4,5]
+    mbx.delete 1
+    results = mbx.read_all
+    assert_equal 4,results.length
+    assert_equal 5,mbx.top
+    assert_nil mbx.read 1
+
+    # [1,nil,3,nil,5]
+    mbx.delete_by_id id3
+    results = mbx.read_all
+    assert_equal 3,results.length
+    assert_equal 5,mbx.top
+    assert_nil mbx.read 3
+
+    # [1,nil,3,nil,nil] => [1,nil,3]
+    mbx.delete 4
+    results = mbx.read_all
+    assert_equal 2,results.length
+    assert_equal 3,mbx.top
+    assert_nil mbx.read 4
+
+    # [1,nil,3,6]
+    id6 = (mbx.store h2("6"), "hello_N6")[:id]
+    results = mbx.read_all
+    assert_equal 3,results.length
+    assert_equal 4,mbx.top
+    assert_equal 3, mbx.find_by_id(id6).first
   end
 
 end
