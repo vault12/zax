407074f430e591b125a0f31ce1138a93f9fdf197
Michael Angerman <stormasm@gmail.com>
2015-08-31 13:46:34 -0700

    code cleanup session_controller_test

--- test/controllers/session_controller_test.rb
+++ test/controllers/session_controller_test.rb
@@ -23,24 +23,24 @@ test "verify session token" do
 
   ### debug
   pclient_token = b64enc @client_token
-  print 'client token = ', pclient_token; puts
-  print 'relay token = ', lines[0]; puts
+  #print 'client token = ', pclient_token; puts
+  #print 'relay token = ', lines[0]; puts
   ### end debug
 
   @relay_token = b64dec lines[0]
   h2_client_token = h2(@client_token)
 
   ### debug
-  ph2_client_token = b64enc h2_client_token
-  print 'h2 client token = ', ph2_client_token; puts
+  #ph2_client_token = b64enc h2_client_token
+  #print 'h2 client token = ', ph2_client_token; puts
   ### end debug
 
   client_relay = concat_str(@client_token,@relay_token)
   h2_client_relay = h2(client_relay)
 
   ### debug
-  ph2_client_relay = b64enc h2_client_relay
-  print 'h2 client relay = ', ph2_client_relay; puts
+  #ph2_client_relay = b64enc h2_client_relay
+  #print 'h2 client relay = ', ph2_client_relay; puts
   ### end debug
 
   _raw_post :verify_session_token, {}, h2_client_token, h2_client_relay
@@ -48,50 +48,11 @@ test "verify session token" do
   body = response.body
   lines = _check_body(body)
   ### debug
-  print 'session key xor client token = ', lines[0]; puts
+  #print 'session key xor client token = ', lines[0]; puts
   ### end debug
   skxorct = b64dec lines[0]
   session_key = xor_str(skxorct,@client_token)
-  print 'session key = ', "#{b64enc session_key}"; puts
+  #print 'session key = ', "#{b64enc session_key}"; puts
 
 end
-
 end
-
-=begin
-test "verify_session_token guards" do
-  head :verify_session_token
-  _fail_response :precondition_failed # missing header
-
-  @request.headers["HTTP_#{TOKEN}"] = rand_bytes 32
-  head :verify_session_token
-  _fail_response :precondition_failed # wrong encoding
-
-  @request.headers["HTTP_#{TOKEN}"] = b64enc rand_bytes 32
-  head :verify_session_token
-  _fail_response :precondition_failed # wrong token
-end
-
-test "token is consitent until timeout" do
-  token = b64enc rand_bytes 32
-  @request.headers["HTTP_#{TOKEN}"] = token
-  get :new_session_token
-  _success_response
-  counter_token = Base64.decode64(response.body)
-
-  @request.headers["HTTP_#{TOKEN}"] = token
-  get :new_session_token
-  _success_response
-  assert_equal(Base64.decode64(response.body), counter_token)
-  #
-  # Comment out temporarily so that tests run faster
-  #
-
-  ...THIS ALL GETS COMMENTED OUT BELOW HERE...
-
-  sleep Rails.configuration.x.relay.token_timeout
-  @request.headers["HTTP_#{TOKEN}"] = token
-  post :verify_session_token
-  logger.info "h:"+@response.headers["X-Error-Details"]
-  _fail_response :precondition_failed # timed out
-=end
