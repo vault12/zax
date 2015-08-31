3455d1a2066b2c78f1b395345d37b095b65e6295
Michael Angerman <stormasm@gmail.com>
2015-08-31 12:40:17 -0700

    session_test is now working

--- test/integration/session_test.rb
+++ test/integration/session_test.rb
@@ -2,32 +2,29 @@ require 'test_helper'
 
 class VerifySessionTest < ActionDispatch::IntegrationTest
   test "verify session token flow 01" do
-    requst_token = b64enc RbNaCl::Random.random_bytes 32
-    get "/session", nil, "HTTP_#{TOKEN}": requst_token
+
+    post "/start_session", nil
+    _fail_response :internal_server_error # wrong token
+
+    @client_token = RbNaCl::Random.random_bytes 32
+    _post "/start_session", @client_token
     _success_response
-    random_token = b64dec response.body
 
-    post "/session", nil, "HTTP_#{TOKEN}": (b64enc RbNaCl::Random.random_bytes 32)
-    _fail_response :precondition_failed # wrong token
+    @client_token = RbNaCl::Random.random_bytes 31
+    _post "/start_session", @client_token
+    _fail_response :internal_server_error # wrong token
 
-    post "/session", "hello"*100,
-      'CONTENT_TYPE':'application/text',
-      "HTTP_#{TOKEN}": requst_token
-    _fail_response :conflict # handshake mismatch
+    @client_token = b64enc RbNaCl::Random.random_bytes 32
+    _post "/start_session", @client_token
+    _fail_response :internal_server_error
 
-    post "/session", requst_token,
-      'CONTENT_TYPE':'application/text',
-      "HTTP_#{TOKEN}": requst_token
-    _fail_response :conflict # still wrong handshake
+    _post "/start_session", "hello vault12"
+    _fail_response :internal_server_error
 
-    real_handshake = random_token.bytes.zip((Base64.decode64 requst_token).bytes).map { |a,b| a^b }.pack("C*")
-
-    post "/session", b64enc(real_handshake),
-      'CONTENT_TYPE':'application/text',
-      "HTTP_#{TOKEN}": requst_token
+    @client_token = RbNaCl::Random.random_bytes 32
+    _post "/start_session", @client_token
     _success_response
     pkey = b64dec response.body
     assert_not_empty pkey
   end
-
 end
