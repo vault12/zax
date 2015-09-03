feb256816777653be82082bfc9280f8ee9d61c16
Michael Angerman <stormasm@gmail.com>
2015-09-03 13:19:19 -0700

    working on storing multiple hpks in redis

---
+++ test/mailbox/hpk_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+require 'prove_test_helper'
+
+class MultipleHpkTest < ProveTestHelper
+  test "multiple hpk storage in redis" do
+    hpk = setup_prove
+    _setup_keys hpk
+    to_hpk = RbNaCl::Random.random_bytes(32)
+    to_hpk = b64enc to_hpk
+    data = {cmd: 'upload', to: to_hpk, payload: 'hello world 0'}
+    n = _make_nonce
+    _post "/command", hpk, n, _client_encrypt_data(n,data)
+  end
+end
