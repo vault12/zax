da9df591de9c665240ade14ee451b3f70a9211b9
Max Skibinsky <max@skibinsky.com>
2015-09-10 23:11:02 -0700

    detailed comments

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -3,18 +3,29 @@ class SessionController < ApplicationController
 
   # POST /start_session - start handshake
   def start_session_token
-    @body = request.body.read SESSION_START_BODY
-    lines = _check_body_lines @body, 1, 'start session'
+    # ready only number of bytes we expect
+    body = request.body.read SESSION_START_BODY
+
+    # we expect 1 line to start session
+    lines = _check_body_lines body, 1, 'start session'
+
+    # check that client token is valid
     @client_token = _check_client_token lines[0]
+
+    # lets make relay token to initiate handshake
     @relay_token  = _make_relay_token
+
+    # save both tokens to redis
     _cache_tokens
 
     # Send back our token as base64 response body
     render text: b64enc(@relay_token)
 
   rescue ZAXError => e
-    e.http_fail
+    # handle known protocol errors
+    e.http_fail 
   rescue => e
+    # That is unexpected, fail the request
     logger.error "#{ERROR} start_session_token error: #{e}"
     head :internal_server_error, x_error_details:
       'Relay error: try again later'
@@ -22,29 +33,34 @@ class SessionController < ApplicationController
 
   # POST /verify_session - verify started handshake
   def verify_session_token
-    @body = request.body.read SESSION_VERIFY_BODY
-    lines = _check_body_lines @body, 2, 'verify session'
+    # read only bytes we expect for verification
+    body = request.body.read SESSION_VERIFY_BODY
+
+    # original token and signature in 2 lines
+    lines = _check_body_lines body, 2, 'verify session'
+
+    # check that client token line is valid
     _check_client_token lines[0]
-    decode = lines.map do |l|
-      if l.length == TOKEN_B64
-        b64dec l
-      else
-        fail ReportError.new self,
-          msg:"verify_session lines wrong size, expecting #{TOKEN_B64}"
-      end
-    end
+
+    # decode and check both lines
+    decode = _decode_lines lines
 
     # check handshake or throw error
     # returns client_token and its h2() from cache
     ct, h2_ct = _verify_handshake(decode)
+    
+    # signature is valid, lets make temp keys
     session_key = _make_session_key h2_ct
 
-    # resond with relay temp key for this session masked by client token
+    # resond with relay temp key for this session
+    # masked by client token
     render text: "#{b64enc(xor_str(session_key.public_key.to_bytes, ct))}"
 
   rescue ZAXError => e
+    # handle known protocol errors
     e.http_fail
   rescue => e
+    # unexpected errors, such as encoding, etc.
     logger.warn "#{WARN} verify_session_token error:\n"\
       "#{EXPT} #{e}"
     head :conflict, x_error_details: 'Provide session handshake: h2(client,relay)'
@@ -52,8 +68,10 @@ class SessionController < ApplicationController
 
   # ----- Private helper functions ----
   private
+   # runs before every response
    def _preamble
-    expires_now # never cache
+    # never cache
+    expires_now 
    end
 
    def _make_relay_token
@@ -74,6 +92,17 @@ class SessionController < ApplicationController
     Rails.cache.write "relay_token_#{h2_client_token}",
       @relay_token,
       expires_in: Rails.configuration.x.relay.token_timeout
+  end
+
+  def _decode_lines(lines)
+    lines.map do |l|
+      if l.length == TOKEN_B64
+        b64dec l
+      else
+        fail ReportError.new self,
+          msg:"verify_session lines wrong size, expecting #{TOKEN_B64}"
+      end
+    end
   end
 
   def _load_cached_tokens(h2_ct)
